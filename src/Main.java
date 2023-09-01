5803e3e1f0fa2bec1d95b850914a9e12efc26f3a
AQIL TIJANI <100186541@lubbockisd.net>
2023-09-01 09:39:31 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("                 _  _\n" +
+              "                | )/ )\n" +
+              "             \\\\ |//,' __");
+      System.out.println("             (\")(_)-\"()))=-\n" +
+              "                (\\\\");
+      System.out.println("                             _   _\n" +
+              "  HEELP                     ( | / )");
+      System.out.println("                         \\\\ \\|/,' __\n" +
+              "    \\_o_/                 (\")(_)-\"()))=-");
+      System.out.println("       )                     <\\\\\n" +
+              "      /\\__");
+      System.out.println("_____ \\ ________________________________");
+    }
+}
